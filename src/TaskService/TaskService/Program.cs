9db3251d93191a2c25665376a6811e0718fa88e9
jesus.gil <jesus.gil@thoughtworks.com>
2025-08-04 12:19:48 +0200

    Add Swagger

--- src/TaskService/TaskService/Program.cs
+++ src/TaskService/TaskService/Program.cs
@@ -1,15 +1,20 @@
 using TaskService.Domain;
 using TaskService.Infrastructure;
+using Microsoft.Extensions.DependencyInjection;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 // Inyección del repo en memoria
 builder.Services.AddSingleton<ITaskRepository, InMemoryTaskRepository>();
 
 var app = builder.Build();
+
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.MapControllers();
 
